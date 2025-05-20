f77ee46948c203e9211c529d2305dc50496f9a81
FadhliRajwaa <fadhlirajwaarahmana@gmail.com>
2025-05-20 21:21:25 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+// Gunakan HashRouter saat di production untuk menghindari masalah routing pada server static
+// BrowserRouter memerlukan konfigurasi server side untuk menangani routing
+const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <ThemeProvider>
         <App />
         <ToastContainer position="top-right" autoClose={3000} />
       </ThemeProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
